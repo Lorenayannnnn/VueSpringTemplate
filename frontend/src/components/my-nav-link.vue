90d63e55110fbf00afe25fb49b2acb725e66fbb6
Lorenayannnnn <lorenayan@qq.com>
2021-10-24 12:50:02 -0700

    updateFrontend

---
+++ frontend/src/components/my-nav-link.vue
@@ -0,0 +1,29 @@
+<template>
+  <router-link ref="myRouterLink" class="navLink" :to="to"><slot/></router-link>
+</template>
+
+<script>
+export default {
+  name: "my-nav-link",
+  props: {
+    to: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.navLink {
+  text-decoration: none;
+  font-weight: 700;
+  margin: 5px;
+  color: #4d4e50;
+  transition: color .2s cubic-bezier(.645,.045,.355,1);
+}
+.navLink:hover {
+  color: #FF8626;
+  transition: color .2s cubic-bezier(.645,.045,.355,1);
+}
+</style>
